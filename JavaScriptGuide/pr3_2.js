db45b3cd55999283b5dff0f66d4a4d913351fae9
DoddsDonnelly <crystaldodds@msn.com>
2016-08-19 09:23:44 -0700

    Chapter 3 complete.

---
+++ JavaScriptGuide/pr3_2.js
@@ -0,0 +1,11 @@
+﻿var myheading = "<h1>This is My Web Page!</h1>",
+linktag = "<a href=\"http://www.webpertz.net\">Web Site Link!</a>",
+sometext = "This text can be affected by other statements.",
+//begineffect = "<em>", endeffect = "</em>",
+begineffect = "<strong>", endeffect = "</strong>",
+beginpara = "<p>", endpara = "</p>";
+
+document.write(myheading);
+document.write(begineffect + sometext + endeffect);
+document.write(beginpara + linktag + endpara);
+document.write(beginpara + sometext + endpara);
