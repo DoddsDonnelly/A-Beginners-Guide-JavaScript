5a20d36846853ea7b58b6f19feb6e69791ea6798
DoddsDonnelly <crystaldodds@msn.com>
2016-08-19 09:47:36 -0700

    Chapter 4 Complete

---
+++ JavaScriptGuide/pr4_1.js
@@ -0,0 +1,14 @@
+﻿function car_cost(mycar, paycheck) {
+    window.alert("You have a " + mycar + " and make $" + paycheck);
+}
+
+function get_added_text() {
+    var string1 = "This project ";
+    var string2 = "is almost fun!";
+    var addStrings = string1 + string2;
+    return addStrings;
+}
+
+car_cost("Mustang", 1500);
+var alerttext = get_added_text();
+window.alert(alerttext);
---
+++ JavaScriptGuide/pr4_2.js
@@ -0,0 +1,8 @@
+﻿function dispaly_HTML() {
+    var myheading = "<h1>Hello, World!</h1>";
+    var mytext =
+        "<p>While it is nice to know you world, these are only some things that I am comfortable sharing in a global context.  You can't alter the variable that holds this text outside of the function that contains it! Ha!</p>";
+    document.write(myheading + mytext);
+}
+
+dispaly_HTML();
