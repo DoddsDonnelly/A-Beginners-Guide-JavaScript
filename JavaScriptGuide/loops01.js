de7ca9213014d7f60b5c8c830f952e48ab3c41a4
DoddsDonnelly <crystaldodds@msn.com>
2016-08-22 14:47:48 -0700

    Chapter 6 complete

---
+++ JavaScriptGuide/loops01.js
@@ -0,0 +1,83 @@
+﻿// Example 1
+document.write("Get ready for some repeated text. <br>");
+for (var count = 1; count < 11; count++) {
+    document.write(count + ". I am part of a loop!<br>");
+}
+document.write("Now we are back to the plain text.");
+
+// Example 2
+document.write("<br><br><br>");
+for (var count = 1; count < 11; count++) {
+    document.write("I am part of a loop!<br>");
+    for (var nestcount = 1; nestcount < 3; nestcount++) {
+        document.write("I keep interrupting in pairs!<br>");
+    }
+}
+document.write("Now we are back to the plain text.");
+
+// Example 3
+document.write("<br><br><br>");
+for (var count = 1; count < 11; count++) {
+    if (count === 5) {
+        document.write("The loop is halfway done!<br>");
+    } else
+        document.write("I am part of a loop!<br>");
+}
+document.write("Now we are back to the plain text.");
+
+// Example 4 (not from book)
+document.write("<br><br><br>");
+for (var count = 1; count < 100; count++) {
+    if (count % 3 === 0) {
+        document.write("Fizz<br>");
+    } else if (count % 5 === 0) {
+        document.write("Buzz<br>");
+    } else
+        document.write(count + "<br>");
+}
+document.write("Now we are back to the plain text.");
+
+// Example 5
+document.write("<br><br><br>");
+document.write("Get ready for some repeated text. <br>");
+var count = 1;
+while (count < 11) {
+    document.write(count + ". I am part of a loop!<br>");
+    count++;
+}
+document.write("Now we are back to the plain text.");
+
+// Example 6
+document.write("<br><br><br>");
+document.write("Get ready for some repeated text. <br>");
+var count = 1;
+do {
+    document.write(count + ". Hi<br>");
+    count++;
+} while (count < 10);
+document.write("Now we are back to the plain text.");
+
+// Example 7
+document.write("<br><br><br>");
+var stopnumber = 5;
+for (var count = 1; count < 11; count++) {
+    if (count === stopnumber) {
+        document.write("Sorry, the loop stops here!<br>");
+        break;
+    } else {
+        document.write(count + ". I am part of a loop!<br>");
+    }
+}
+document.write("Now we are back to the plain text.");
+
+// Example 8
+document.write("<br><br><br>");
+var skipnumber = 5;
+for (var count = 1; count < 11; count++) {
+    if (count === skipnumber) {
+        document.write("Loop doesn't Stop <br>");
+        continue;
+    }
+    document.write(count + ". I am part of a loop!<br>");
+}
+document.write("Now we are back to the plain text.");
---
+++ JavaScriptGuide/pr6_1.js
@@ -0,0 +1,20 @@
+﻿var msg = "", city = window.prompt("What is the name of your city?", "");
+if ((city === null) || (city === "")) {
+    msg = "No city entered.  Cannot determine delivery availability.";
+} else {
+    switch (city) {
+        case "Johnstown":
+            msg = "We can have items delivered to you in 3 days.";
+            break;
+        case "Donille":
+            msg = "We can have items delivered to you in 1 week.";
+            break;
+        case "Danieltown":
+        case "Martyville":
+            msg = "We can have items delivered to you in 2 week.";
+            break;
+        default:
+            msg = "Sorry!  We do not deliver to your city yet.";
+    }
+}
+document.write(msg);
