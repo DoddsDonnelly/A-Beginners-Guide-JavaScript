30037b0888ff8d999fd2c1bb86b91e345d12960f
Dodds-Donnelly <doddsdonnelly@yahoo.com>
2016-08-29 09:33:26 -0700

    Chapter 7 - Complete.

---
+++ JavaScriptGuide/pr7_1.js
@@ -0,0 +1,4 @@
+﻿var computer_parts = ["Monitors", "Motherboards", "Chips", "Hard Drives", "DVD-ROMs", "Cases", "Power Supplies"];
+for (var i = 0; i < computer_parts.length; i++) {
+    document.write(computer_parts[i] + "<br>");
+}
