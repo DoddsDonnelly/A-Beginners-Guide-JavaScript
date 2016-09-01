e9c4d4d0900efb785db8ae3d69e13e65be647a6e
Dodds-Donnelly <doddsdonnelly@yahoo.com>
2016-09-01 09:29:37 -0700

    Chapter 8 - Complete

---
+++ JavaScriptGuide/pr8_1.js
@@ -0,0 +1,7 @@
+﻿var computer =
+{
+    speed: "4GHZ",
+    hd: "500GB",
+    ram: "8GB"
+};
+document.write("Computer Speed: " + computer.speed + "<br>Computer Hard Disk: " + computer.hd + "<br>Computer RAM: " + computer.ram);
---
+++ JavaScriptGuide/pr8_2.js
@@ -0,0 +1,23 @@
+﻿function Computer(type, processor, ram, hd) {
+    this.type = type;
+    this.processor = processor;
+    this.ram = ram;
+    this.hd = hd;
+
+}
+
+Computer.prototype.describe = function (){
+    document.write("<p>" + this.type + "<br>");
+    document.write("Processor: " + this.processor + "<br>");
+    document.write("Ram: " + this.ram + "<br>");
+    document.write("Hard Disk: " + this.hd + "</p>");
+};
+
+var work_computer = new Computer("Work", "2GHZ", "8GB", "1TB");
+var home_computer = new Computer("Home", "2GHZ", "4GB", "500GB");
+var gaming_computer = new Computer("Gaming", "4GHZ", "16GB", "2TB");
+
+work_computer.describe();
+home_computer.describe();
+gaming_computer.describe();
+
