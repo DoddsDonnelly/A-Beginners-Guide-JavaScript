872c988131497cc2eca86419581fdc9a3d42f1a4
Dodds-Donnelly <doddsdonnelly@yahoo.com>
2016-09-19 11:07:58 -0700

    Chapter 13 - Complete

---
+++ JavaScriptGuide/pr13_1.js
@@ -0,0 +1,10 @@
+﻿var getemail = document.getElementById("getemail");
+getemail.addEventListener("submit", function(event) {
+    var email_add = document.getElementById("email").value;
+    event.preventDefault();
+    if ((email_add.indexOf("@") >= 0) && (email_add.indexOf(".") >= 0)) {
+        alert("Thank you!");
+    } else {
+        alert("The @ and . characters are required in the e-mail address");
+    }
+}, false)
---
+++ JavaScriptGuide/pr13_2.js
@@ -0,0 +1,25 @@
+﻿function set_it() {
+    var name = prompt("Enter your name.", ""),
+        thetext = "name=" + name,
+        expdate = ";expires=Mon, 19 Sept 2016 13:00:00 UTC",
+        newtext = escape(thetext);
+    newtext += expdate;
+    document.cookie = newtext;
+}
+
+function read_it() {
+    var mycookie = document.cookie,
+        fixed_cookie = unescape(mycookie),
+        thepair = fixed_cookie.split("="),
+        uname = thepair[1],
+        greeting = document.getElementById("greeting");
+    greeting.innerHTML = "Hello, " + uname;
+}
+
+if (document.cookie) {
+    read_it();
+}
+else {
+    set_it();
+    read_it();
+}
