fef20fbfeecd3ffdd518ad37b91a5f3f72e8419d
Dodds-Donnelly <doddsdonnelly@yahoo.com>
2016-09-26 09:19:54 -0700

    Chapter 15 - Complete

---
+++ JavaScriptGuide/Problems/pr15_1.js
@@ -0,0 +1,22 @@
+﻿var im = document.getElementById("pic1"),
+    imlink = document.getElementById("imlink");
+
+//im.addEventListener("mouseover", function() {
+//    im.src = "../Images/2.png";
+//}, false);
+//im.addEventListener("mouseout", function () {
+//    im.src = "../Images/1.png";
+//}, false);
+
+function on_image() {
+    im.src = "../Images/2.png";
+}
+
+function off_image() {
+    im.src = "../Images/1.png";
+}
+
+im.addEventListener("mouseover", on_image, false);
+im.addEventListener("mouseout", off_image, false);
+imlink.addEventListener("focus", on_image, false);
+imlink.addEventListener("blur", off_image, false);
---
+++ JavaScriptGuide/Problems/pr15_2.js
@@ -0,0 +1,5 @@
+﻿var slink = document.getElementById("slink");
+slink.addEventListener("click", function() {
+    event.preventDefault();
+    window.open('sales.html', 'newwin', 'width=300, height=200');
+}, false);
