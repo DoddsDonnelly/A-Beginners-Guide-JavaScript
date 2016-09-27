2dc26144a12b58219b512264adf09186dacb045e
Dodds-Donnelly <doddsdonnelly@yahoo.com>
2016-09-27 09:03:16 -0700

    Book Complete

---
+++ JavaScriptGuide/Problems/pr16_1.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $("#story").hide();
+    $("#changelink").click(function (event) {
+        event.preventDefault();
+        $("#story").slideDown(3000, function () {
+            $("#story").fadeOut(10000);
+        });
+    });
+});
---
+++ JavaScriptGuide/Problems/pr16_2.js
@@ -0,0 +1,16 @@
+﻿var dragdiv = document.getElementById("mylink"),
+    dropdiv = document.getElementById("dropdiv");
+dropdiv.addEventListener("dragstart", function (event) {
+    event.dataTransfer.setData("text", "This link goes to my Web Site!");
+}, false);
+dropdiv.addEventListener("dragover", function (event) {
+    event.preventDefault();
+}, false);
+dropdiv.addEventListener("dragenter", function (event) {
+    event.preventDefault();
+}, false);
+dropdiv.addEventListener("drop", function (event) {
+    var divtext = event.dataTransfer.getData("Text");
+    event.preventDefault();
+    dropdiv.innerHTML = divtext;
+}, false);
